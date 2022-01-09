e8631057c19ac35f58eb9ec95335835799aff232
danieltorquato <danieltorquato2009@gmail.com>
2022-01-09 17:18:32 -0300

    first commit

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from twilio.rest import Client
 
 #Twillio -> integração Python com SMS
 # Your Account SID from twilio.com/console
-account_sid = "AC80ac59655b3b676a91057a8bc8c9bc3c"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "758fe87d5d5a06d6947d57338f8d32ff"
+auth_token  = ""
 client = Client(account_sid, auth_token)
 #Abrir 6 arquivos em Excel
 lista_meses=["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"]
@@ -24,7 +24,7 @@ for mes in lista_meses:
         print(f'No mês de {mes} há um vendedor com mais de R$55.000. Vendedor: {vendedor}, Vendas:{vendas}')
 #Se for maior que R$55.000 -> Envia um SMS com o Nome, Mês e vendas do vendedor
         message = client.messages.create(
-            to="+5511953898557",
+            to="+55",
             from_="+17604932840",
             body=f"O Vendedor {vendedor} conseguiu bater a meta de {vendas} em vendas")
 
